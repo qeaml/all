fd95baebf4b96f27c0bf7926e92905e907a85fb0
qeaml <qeaml@protonmail.com>
2022-03-04 20:04:27 +0100

    add Partial3

--- funcs/funcs.go
+++ funcs/funcs.go
@@ -21,3 +21,15 @@ func Partial[T1 any, T2 any, R any](t1 T1, f func(T1, T2) R) func(T2) R {
 		return f(t1, t2)
 	}
 }
+
+func Partial3With2[T1 any, T2 any, T3 any, R any](t1 T1, t2 T2, f func(T1, T2, T3) R) func(T3) R {
+	return func(t3 T3) R {
+		return f(t1, t2, t3)
+	}
+}
+
+func Partial3With1[T1 any, T2 any, T3 any, R any](t1 T1, f func(T1, T2, T3) R) func(T2, T3) R {
+	return func(t2 T2, t3 T3) R {
+		return f(t1, t2, t3)
+	}
+}
